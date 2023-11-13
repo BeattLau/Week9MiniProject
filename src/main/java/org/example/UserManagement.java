709e7dbe716dd18200feb7b03314eb847f61d929
BeattLau <144425291+BeattLau@users.noreply.github.com>
2023-11-13 08:42:20 +0100

    updated userManagement to clear error in CLIMenu

--- src/main/java/org/example/UserManagement.java
+++ src/main/java/org/example/UserManagement.java
@@ -1,12 +1,13 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserManagement {
-    private static List<User> userList;
+    private List<User> userList;
 
     public UserManagement(List<User> userList) {
-        this.userList = userList;
+        this.userList = new ArrayList<>();
     }
 
     public void addUser(User user) {
@@ -14,7 +15,7 @@ public class UserManagement {
         System.out.println("User added successfully.");
     }
 
-    public static void updateUser(String userName, String newPassword) {
+    public void updateUser(String userName, String newPassword) {
         for (User user : userList) {
             if (user.getUsername().equals(userName)) {
                 user.setPassword(newPassword);
@@ -25,13 +26,12 @@ public class UserManagement {
         System.out.println("User not found. Update failed.");
     }
 
-
-    public static void removeUser(User user) {
+    public void removeUser(User user) {
         userList.remove(user);
         System.out.println("User removed successfully.");
     }
 
-    public static User searchForUser(String username) {
+    public User searchForUser(String username) {
         for (User user : userList) {
             if (user.getUsername().equals(username)) {
                 return user;
@@ -40,7 +40,7 @@ public class UserManagement {
         return null;
     }
 
-    public static void displayUsers() {
+    public void displayUsers() {
         System.out.println("All users: ");
         for (User user : userList) {
             System.out.println("Username: " + user.getUsername() + ", Password: " + user.getPassword());
